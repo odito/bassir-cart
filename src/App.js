6957e5e7a214bd7b609dc653390a7256f9101644
odito <odkolas@gmail.com>
2020-08-24 19:09:52 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+   
+      <div className="grid-container">
+        <header>
+          <a href="/">React shopping cart</a>
+        </header>
+        <main>
+          product list
+        </main>
+
+        <footer>
+          All rights reserved
+        </footer>
+      </div>
+
+  );
+}
+
+export default App;
